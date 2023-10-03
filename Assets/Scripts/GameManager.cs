f674e4d6e6f498852e9715d4a6cbd1e79c0f1807
majidmoghadam <majid.moghadam@gmail.com>
2023-10-03 15:22:19 -0400

    chapter 5 - added comments to the Game Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -27,53 +27,78 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //currentSpawnTime time passed since last spawn call
         currentSpawnTime += Time.deltaTime;
 
+        //condition to generate a new wave of Aliens
         if (currentSpawnTime > generatedSpawnTime)
         {
+            //resets the timer after a spawn occurs
             currentSpawnTime = 0;
+
+            //spawn-time randomizer
             generatedSpawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+
+            //ensures number of aliens within limits
             if (aliensPerSpawn > 0 && aliensOnScreen < totalAliens)
             {
+                //this List keeps track of where you have already spawned Aliens
                 List<int> previousSpawnLocations = new List<int>();
+
+                //limits number of Aliens to number of Spawnpoints
                 if (aliensPerSpawn > spawnPoints.Length)
                 {
                     aliensPerSpawn = spawnPoints.Length - 1;
                 }
 
+                //preventative code to make sure you do not spawn more aliens
+                //than you've configured.
                 aliensPerSpawn = (aliensPerSpawn > totalAliens) ?
                     aliensPerSpawn - totalAliens : aliensPerSpawn;
 
+                //this code loops once for each spawned Alien
                 for (int i = 0; i < aliensPerSpawn; i++)
                 {
                     if (aliensOnScreen < maxAliensOnScreen)
                     {
+                        //keeps track of numb of aliens spawned
                         aliensOnScreen += 1;
-                        // 1
+                        // value of -1 means no index has been assigned or found
+                        //for the spawnpoint
                         int spawnPoint = -1;
-                        // 2
+                        // while loop keeps looking for a spawning point (index)
+                        //that has not been used yet
                         while (spawnPoint == -1)
                         {
-                            // 3
+                            // create random index of List(array) between 0 and number of spawnpoints
                             int randomNumber = Random.Range(0, spawnPoints.Length - 1);
-                            // 4
+                            // check to see if random spawnpoint has not already been used
                             if (!previousSpawnLocations.Contains(randomNumber))
                             {
+                                //add this random number to the List
                                 previousSpawnLocations.Add(randomNumber);
+                                //use this random number for the spawn location index
                                 spawnPoint = randomNumber;
                             }
                         }
 
+                        //actual point(label) on arena to apawn next alien
                         GameObject spawnLocation = spawnPoints[spawnPoint];
 
+                        //code to actually create a new Alien from a Prefab
                         GameObject newAlien = Instantiate(alien) as GameObject;
 
+                        //position the new alien to that random unused spawnede point
                         newAlien.transform.position = spawnLocation.transform.position;
 
+                        //get the "Alien" code from the new Alien spawned
                         Alien alienScript = newAlien.GetComponent<Alien>();
 
+                        //set the new alien target to where the player currently is
+                        //NOTE: GameManager code affecting Alien code.
                         alienScript.target = player.transform;
 
+                        //the new Aliens turn towards the player
                         Vector3 targetRotation = new Vector3(player.transform.position.x,
                                                 newAlien.transform.position.y, player.transform.position.z);
                         newAlien.transform.LookAt(targetRotation);
