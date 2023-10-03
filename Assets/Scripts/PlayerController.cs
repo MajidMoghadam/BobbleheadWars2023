77722d2aee65b673a399736a59d0933ab1c76970
majidmoghadam <majid.moghadam@gmail.com>
2023-10-02 20:08:33 -0400

    Chapter 4 - Added Character Controller to Space Marine

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,26 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private CharacterController characterController;
     public float moveSpeed = 50.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //create a temp variable to store 3D positional information
-        Vector3 pos = transform.position;
-        //get the user input from the keyboard (or whatever Input)
-        //and store it in this variable
-        pos.x += moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
-        pos.z += moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
-        
-        //apply this user input to the space marine
-        //so the space marine actually moves
-        transform.position = pos;
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"),
+                                        0, Input.GetAxis("Vertical"));
+        characterController.SimpleMove(moveDirection * moveSpeed);
     }
 }
